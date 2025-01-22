f9035ad62c34101cb532cc12d520b922ec265276
Omar Huweih <ohuweih@gmail.com>
2025-01-22 14:43:44 -0800

    Adding everything to a folder

---
+++ wordToAsciiConverter/main.py
@@ -0,0 +1,96 @@
+import os
+import subprocess 
+import argparse
+import logging
+import formatting
+import imageConverter
+
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    handlers=[logging.FileHandler("my_log_file.log", mode='w', encoding='utf-8'),
+                              logging.StreamHandler()])
+
+def run_pandoc(media_folder, input_file, output_file): 
+    """ 
+    Run a Windows shell command and save the output to a file. 
+    
+        Args: command (str): The shell command to run. 
+        output_file (str): Path to the file where output will be saved. 
+    """ 
+    try:
+        command = f"/Users/ohuweih/Documents/pandoc-3.6.2-windows-x86_64/pandoc-3.6.2/pandoc.exe -f docx -t asciidoc --default-image-extension '.png' --extract-media={media_folder} -o {output_file[:-15]}/{output_file} {input_file}"
+        subprocess.run(command) 
+        print(f"Command executed successfully. Output saved to {output_file}") 
+    except subprocess.CalledProcessError as e: 
+        print(f"Failed to execute command: {e}") 
+        
+
+def process_content(content):
+    logging.info("Removing certain patterns in asciidoc file")
+    content = formatting.remove_text_by_patterns(content)
+
+    logging.info("changing all image links to .png")
+    content = formatting.replace_image_suffix_to_png(content)
+
+    logging.info("Escaping double angle brackets")
+    content = formatting.escape_double_angle_brackets(content)
+
+    logging.info("Styling note boxes")
+    content = formatting.recolor_notes(content)
+
+    logging.info("Adding anchors to bibliography")
+    keys, content = formatting.add_anchors_to_bibliography(content)
+
+    logging.info("Connecting in-document references to bibliography")
+    content = formatting.add_links_to_bibliography(content, keys)
+
+    logging.info("Fixing image captions")
+    content = formatting.use_block_tag_for_img_and_move_caption_ahead(content)
+
+    logging.info("Escaping square brackets")
+    content = formatting.escape_source_square_brackets(content)
+
+    logging.info("Removing bad ++ patters")
+    content = formatting.remove_bad_plus_syntax(content)
+
+    return content
+
+
+def write_output(output_file, content):
+    logging.info(f"Writing fixed content to the output file: {output_file}")
+    with open(output_file, 'w', encoding="utf-8") as file:
+        file.write(content)
+
+
+def fix_asciidoc(input_file, output_file):
+    #directory = pathlib.Path(input_file).parent
+
+    logging.info("Read the initial asciidoc file...")
+    with open(input_file, 'r', encoding="utf-8") as file:
+        content = file.read()
+
+    content = process_content(content)
+    write_output(f"{output_file[:-5]}/{output_file}", content)
+    os.remove(input_file) 
+
+
+def main():
+    '''
+    Main fuction will take two arguments (input_file and output_file) and run a docx to asciidoc conversion of the file, put all media in to its own folder "/media/media", edit the asciidoc file to change all .emf strings to .png strings and convert all emf images to png images"
+    
+    '''
+    parser = argparse.ArgumentParser(description="convert docx to adoc, including image support")
+    parser.add_argument("-i", "--input", required=True, help="Docx to convert")
+    parser.add_argument("-t", "--to", required=True, help="name of file to convert to")
+
+    args = parser.parse_args()
+    media_folder = f"{args.to}/extracted_media/"
+
+    run_pandoc(media_folder, args.input, f"{args.to}_no_format.adoc")
+    
+    fix_asciidoc(f"{args.to}/{args.to}_no_format.adoc", f"{args.to}.adoc")
+
+    imageConverter.convert_images_to_png(media_folder)
+
+if __name__ == "__main__":
+    main()
